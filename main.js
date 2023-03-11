20f619f8da441e07aba91145d8c829f8aedf37fd
abdoul aziz diallo <abdoul-aziz.diallo@etu.unistra.fr>
2023-03-11 22:10:11 +0100

    Front page is more smooth

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const hamburger = document.querySelector("header div");
 const navMenu = document.querySelector("header ul");
+const home = document.getElementById("home");
+const frontPage = document.querySelector("section:first-child");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
+  home.classList.toggle("change-top");
+  frontPage.classList.toggle("change-height");
 });

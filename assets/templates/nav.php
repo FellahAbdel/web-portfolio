5ac76f80754768fc72a8a2b38f1b2dae81303805
abdoul aziz diallo <abdoul-aziz.diallo@etu.unistra.fr>
2023-03-13 16:55:43 +0100

    Rename index.html to index.php

--- assets/templates/nav.php
+++ assets/templates/nav.php
@@ -1,6 +1,6 @@
       <nav>
         <ul>
-          <li><a href="/index.html"><?= $trad['nav']['home'] ?></a> </li>
+          <li><a href="/index.php"><?= $trad['nav']['home'] ?></a> </li>
           <li><a href="/formation.html"><?= $trad['nav']['education'] ?></a></li>
           <li><a href="/contact.php"><?= $trad['nav']['contact'] ?></a></li>
         </ul>

9ee11ac36f256b92ea9464044a06ee06514585ec
abdoul aziz diallo <abdoul-aziz.diallo@etu.unistra.fr>
2023-03-13 16:43:46 +0100

    Create nav.php template

---
+++ assets/templates/nav.php
@@ -0,0 +1,12 @@
+      <nav>
+        <ul>
+          <li><a href="/index.html"><?= $trad['nav']['home'] ?></a> </li>
+          <li><a href="/formation.html"><?= $trad['nav']['education'] ?></a></li>
+          <li><a href="/contact.php"><?= $trad['nav']['contact'] ?></a></li>
+        </ul>
+        <div>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </nav>

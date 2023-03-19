f392d8f11dba9da777e0d8e725b0a15771c9842d
abdoul aziz diallo <abdoul-aziz.diallo@etu.unistra.fr>
2023-03-19 15:39:23 +0100

    Add getProjectBetween() method

--- assets/models/Projects.php
+++ assets/models/Projects.php
@@ -18,6 +18,22 @@ class Projects extends Database
     return $statement->fetch();
   }
 
+  public function getProjectBetween(int $startPosition, int $countProject)
+  {
+    $statement = $this->pdo->prepare(
+      'SELECT * FROM projects
+      ORDER BY id
+      LIMIT :start, :countP'
+    );
+
+    $statement->bindValue(':start', $startPosition, PDO::PARAM_INT);
+    $statement->bindValue(':countP', $countProject, PDO::PARAM_INT);
+
+    $statement->execute();
+
+    return $statement->fetch();
+  }
+
   public function getProjects()
   {
     return $this->pdo->query('SELECT * FROM projects')->fetchAll();

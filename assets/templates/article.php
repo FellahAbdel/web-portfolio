9b316a4998fd42981ad353424a97f8abfde5acfb
abdoul aziz diallo <abdoul-aziz.diallo@etu.unistra.fr>
2023-03-19 11:37:27 +0100

    Add article template, which represent a project

---
+++ assets/templates/article.php
@@ -0,0 +1,14 @@
+<li>
+  <img src="/assets/images/projects/alejandro-escamilla-xII7efH1G6o-unsplash.jpg" alt="" />
+  <article>
+    <div>
+      <h2><?= $project["title"] ?> </h2>
+      <p>
+        <?= substr($project["description"], 0, 300) ?>
+      </p>
+    </div>
+    <footer>
+      <a href="/">explore -></a>
+    </footer>
+  </article>
+</li>

4a83a524eae83ae10a78bcccf7d6b1377af32f85
abdoul aziz diallo <abdoul-aziz.diallo@etu.unistra.fr>
2023-03-25 15:22:12 +0100

    Change the it's behaviour

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,12 +1,18 @@
 //* C'est repetitif, il faudra penser à trouver un autre moyen plus compacte
 const hamburger = document.querySelector("header div");
 const navMenu = document.querySelector("header ul");
-const mainDiv = document.querySelector("main > div");
 const main = document.querySelector("main");
+const footer = document.querySelector("footer");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
-  mainDiv.classList.toggle("change-top");
-  main.classList.toggle("change-height");
+  main.classList.toggle("hide");
+  footer.classList.toggle("hide");
+});
+
+//* Submit button
+submitBtn = document.querySelector("form button");
+submitBtn.addEventListener("click", (event) => {
+  event.preventDefault();
 });

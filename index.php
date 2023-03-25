e6c49cb9071eaad241f1af70d33799101dd5c7a7
abdoul aziz diallo <abdoul-aziz.diallo@etu.unistra.fr>
2023-03-25 21:47:14 +0100

    Completly translated

--- index.php
+++ index.php
@@ -1,6 +1,18 @@
 <?php
-require_once './assets/locales/en.php';
-include __DIR__ . '/./assets/models/Projects.php';
+
+// $body = file_get_contents('php://input');
+// $data = json_decode($body, true);
+// $lang = isset($data['lang']);
+
+// $lang = $_POST['lang'];
+
+if (isset($_GET['lang']) && $_GET['lang'] == 'en' || !isset($_GET['lang'])) {
+  require_once './assets/locales/en.php';
+} else {
+  require_once './assets/locales/fr.php';
+}
+
+include __DIR__ . '/assets/models/Projects.php';
 
 $projects = new Projects();
 
@@ -47,42 +59,16 @@ $allProject = $projects->getProjectBetween($start, $count);
   <main>
     <section>
       <div id="home">
-        <div>
-          <ul>
-            <li>
-              <a href=""><i class="mdi mdi-github"></i></a>
-            </li>
-            <li>
-              <a href=""><i class="mdi mdi-twitter"></i></a>
-            </li>
-            <li>
-              <a href=""><i class="mdi mdi-linkedin"></i></a>
-            </li>
-            <li></li>
-          </ul>
-          <svg viewBox="0 0 200 187" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-            <mask id="mask0" mask-type="alpha">
-              <path d="M190.312 36.4879C206.582 62.1187 201.309 102.826 182.328 134.186C163.346 165.547 
-        130.807 187.559 100.226 186.353C69.6454 185.297 41.0228 161.023 21.7403 129.362C2.45775 
-        97.8511 -7.48481 59.1033 6.67581 34.5279C20.9871 10.1032 59.7028 -0.149132 97.9666 
-        0.00163737C136.23 0.303176 174.193 10.857 190.312 36.4879Z" />
-            </mask>
-            <g mask="url(#mask0)">
-              <path d="M190.312 36.4879C206.582 62.1187 201.309 102.826 182.328 134.186C163.346 
-        165.547 130.807 187.559 100.226 186.353C69.6454 185.297 41.0228 161.023 21.7403 
-        129.362C2.45775 97.8511 -7.48481 59.1033 6.67581 34.5279C20.9871 10.1032 59.7028 
-        -0.149132 97.9666 0.00163737C136.23 0.303176 174.193 10.857 190.312 36.4879Z" />
-              <image x="12" y="-13" xlink:href="./assets/images/DSC_0590.png" />
-            </g>
-          </svg>
-        </div>
+        <?php
+        require_once './assets/templates/profile.php'
+        ?>
         <?php
         require_once './assets/templates/homeData.php'
         ?>
       </div>
     </section>
     <section id="projects">
-      <h2>Projects</h2>
+      <h2><?= $trad["projectSection"]["title"] ?></h2>
       <ul>
         <?php foreach ($allProject as $project) : ?>
           <?php include __DIR__ . '/assets/templates/article.php' ?>
@@ -93,6 +79,7 @@ $allProject = $projects->getProjectBetween($start, $count);
   <?php
   require_once './assets/templates/footer.php'
   ?>
+  <script src="./assets/js/shared-js.js?parent=index.php"></script>
   <script src="./assets/js/ajax.js"></script>
   <script src="./assets/js/main.js"></script>
 </body>

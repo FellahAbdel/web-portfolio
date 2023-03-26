86138f4c7eb9c31a23b068c7b94a421fc95becff
abdoul aziz diallo <abdoul-aziz.diallo@etu.unistra.fr>
2023-03-27 00:02:25 +0200

    Add main.js

--- projectItem.php
+++ projectItem.php
@@ -10,9 +10,20 @@ $project = $projects->getProject(intval($projectId));
 <!DOCTYPE html>
 <html lang="en">
 
-<?php
-require_once './assets/templates/head.php'
-?>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@7.1.96/css/materialdesignicons.min.css" />
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono&family=Roboto:wght@700&display=swap" rel="stylesheet" />
+  <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@700&family=Oleo+Script:wght@700&family=Quicksand:wght@300;500;700&display=swap" rel="stylesheet" />
+  <link rel="stylesheet" href="assets/css/shared.css" />
+  <link rel="stylesheet" href="assets/css/project-item.css" />
+  <title>fellah's portfolio</title>
+</head>
 
 <body>
   <header>
@@ -24,11 +35,16 @@ require_once './assets/templates/head.php'
     ?>
   </header>
   <main>
-
+    <article id="home">
+      <img src="./assets/images/projects/<?= $project["imageName"] ?>" alt="">
+      <h1><?= $project["title"] ?></h1>
+      <p><?= $project["description"] ?></p>
+    </article>
   </main>
   <?php
   require_once './assets/templates/footer.php'
   ?>
+  <script src="./assets/js/main.js"></script>
 </body>
 
 </html>

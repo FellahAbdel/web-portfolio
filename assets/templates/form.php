46f62a28e452a07fc26aed981716e9ad862ef719
abdoul aziz diallo <abdoul-aziz.diallo@etu.unistra.fr>
2023-03-26 13:04:18 +0200

    Create form template

---
+++ assets/templates/form.php
@@ -0,0 +1,13 @@
+<form action="">
+  <label for="surname"><?= $trad["contact-second-section"]["label1"] ?></label>
+  <input type="text" name="user-name" id="surname" placeholder="Prénom" />
+
+  <label for="email"><?= $trad["contact-second-section"]["label2"] ?></label>
+  <input type="email" id="email" name="user-email" placeholder="Email" />
+
+  <label for="phone-number"><?= $trad["contact-second-section"]["label3"] ?></label>
+  <input type="tel" id="phone-number" name="user-number" placeholder="Numéro de téléphone" />
+  <label for="user-msg"><?= $trad["contact-second-section"]["label4"] ?></label>
+  <textarea name="" id="user-msg" cols="30" rows="10" placeholder="Rédiger votre message"></textarea>
+  <button><?= $trad["contact-second-section"]["submitBtn"] ?></button>
+</form>

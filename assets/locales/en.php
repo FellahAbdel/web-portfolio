83fd92d84bc9f93ff946ce8df81fe8f6650e564c
abdoul aziz diallo <abdoul-aziz.diallo@etu.unistra.fr>
2023-03-26 13:03:44 +0200

    Translate contact.php page

--- assets/locales/en.php
+++ assets/locales/en.php
@@ -6,4 +6,37 @@ $trad = [
     'education' => 'Education',
     'contact' => 'Contact'
   ],
+  'divHome' => [
+    'h1' => 'Hi, I\'m DIALLO',
+    'h3' => 'Web developer',
+    'p' => 'High level experience in web desing and knowledge, producing quality work.',
+    'a' => 'Discover projects'
+  ],
+  'projectSection' => [
+    "title" => "Projects",
+    "exploreButton" => "Explore"
+  ],
+  'contact-first-section' => [
+    "h2" => "Get in touch",
+    "p" => "I like hearing about you. I’m always here to talk",
+    "div1 h3" => "Let's discuss together",
+    "div1 p1" => "I'm here to help you",
+    "div1 p2" => "abdoulaziz@gmail.com",
+    "div2 h3" => "Office",
+    "div2 p1" => "Come tell me Hi",
+    "div2 p2" => "67000 Strasbourg",
+    "div3 h3" => "Phone number",
+    "div3 p1" => "Mon-Fri from 8AM-5PM",
+    "div3 p2" => "0753103914"
+  ],
+  "contact-second-section" => [
+    "h1" => "Touch with me",
+    "p" => "Thank you for your interest in my profile. Please use this form to 
+            contact me. Quick response assured!",
+    "label1" => "First Name",
+    "label2" => "Email",
+    "label3" => "Phone Number",
+    "label4" => "Your message",
+    "submitBtn" => "submit"
+  ]
 ];
--- assets/locales/fr.php
+++ assets/locales/fr.php
@@ -6,4 +6,37 @@ $trad = [
     'education' => 'Education',
     'contact' => 'Contactez-moi',
   ],
+  'divHome' => [
+    'h1' => "Hey, Je m'appelle DIALLO",
+    'h3' => 'Developpeur Web',
+    'p' => 'Expérience de haut niveau dans le web design et la connaissance, la production de travail de qualité.',
+    'a' => 'Decouvrez mes projets'
+  ],
+  'projectSection' => [
+    "title" => "Mes projets",
+    "exploreButton" => "Explorez"
+  ],
+  'contact-first-section' => [
+    "h2" => "Entrez en contact",
+    "p" => "J’aime entendre parler de vous. Je suis toujours ici pour discuter",
+    "div1 h3" => "Discutez-avec moi",
+    "div1 p1" => "Je suis là pour vous aider",
+    "div1 p2" => "abdoulaziz@gmail.com",
+    "div2 h3" => "Burreau",
+    "div2 p1" => "Venez me dire Bonjour",
+    "div2 p2" => "67000 Strasbourg",
+    "div3 h3" => "Numéro de téléphone",
+    "div3 p1" => "Lun-Vend de 8h-17h",
+    "div3 p2" => "0753103914"
+  ],
+  "contact-second-section" => [
+    "h1" => "CONTACTEZ-MOI",
+    "p" => "Merci de l'intérêt que vous portez a mon profile. Veuillez utiliser ce
+            formulaire pour me contacter. Réponse rapide assurée !",
+    "label1" => "Prénom",
+    "label2" => "Email",
+    "label3" => "Numéro de téléphone",
+    "label4" => "Votre message",
+    "submitBtn" => "Soumettre"
+  ]
 ];

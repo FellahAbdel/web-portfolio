fb5c9d511fd7413a8616153ed27a1288c515ed64
abdoul aziz diallo <abdoul-aziz.diallo@etu.unistra.fr>
2023-03-26 13:08:09 +0200

    Minor change

--- assets/templates/form.php
+++ assets/templates/form.php
@@ -8,6 +8,6 @@
   <label for="phone-number"><?= $trad["contact-second-section"]["label3"] ?></label>
   <input type="tel" id="phone-number" name="user-number" placeholder="Numéro de téléphone" />
   <label for="user-msg"><?= $trad["contact-second-section"]["label4"] ?></label>
-  <textarea name="" id="user-msg" cols="30" rows="10" placeholder="Rédiger votre message"></textarea>
+  <textarea name="" id="user-msg" cols="30" rows="9" placeholder="Rédiger votre message"></textarea>
   <button><?= $trad["contact-second-section"]["submitBtn"] ?></button>
 </form>

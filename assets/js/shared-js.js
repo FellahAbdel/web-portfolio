07a90f52e62e02e114943f9c95e337b1cf7088b4
abdoul aziz diallo <abdoul-aziz.diallo@etu.unistra.fr>
2023-04-01 23:39:28 +0200

    If the url contain an id we add ?lang=.. at the end

--- assets/js/shared-js.js
+++ assets/js/shared-js.js
@@ -6,7 +6,15 @@ const langSelect = document.getElementById("lang");
 
 langSelect.addEventListener("change", (event) => {
   const selectedValue = event.target.value;
+  let url = "/" + parentName;
+
+  const regex = /\/projectItem\.php\?id=[0-9]+/i;
+  if (regex.test(window.location.href)) {
+    url = window.location.href.match(regex)[0] + "&lang=" + selectedValue;
+  } else {
+    url += "?lang=" + selectedValue;
+  }
 
   //* We change here, the page url.
-  window.location.href = "/" + parentName + "?lang=" + selectedValue;
+  window.location.href = url;
 });

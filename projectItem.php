937c21aca7971b304d4c54e7942db520e049aecc
abdoul aziz diallo <abdoul-aziz.diallo@etu.unistra.fr>
2023-04-02 12:57:18 +0200

    Finish adding comment section

--- projectItem.php
+++ projectItem.php
@@ -1,5 +1,14 @@
 <?php
-require_once './assets/locales/en.php';
+$en_select = "";
+$fr_select = "";
+if (isset($_GET['lang']) && $_GET['lang'] == 'en' || !isset($_GET['lang'])) {
+  require_once './assets/locales/en.php';
+  $en_select = "selected";
+} else {
+  require_once './assets/locales/fr.php';
+  $fr_select = "selected";
+}
+
 include __DIR__ . '/./assets/models/Projects.php';
 
 $projectId = $_GET["id"];
@@ -30,9 +39,8 @@ $project = $projects->getProject(intval($projectId));
 <body>
   <header>
     <!--I added this.-->
-    <a href="/index.html">Fellah</a>
+    <a href="/index.php">Fellah</a>
     <?php
-    include './assets/locales/en.php';
     include './assets/templates/nav.php'
     ?>
   </header>
@@ -42,11 +50,11 @@ $project = $projects->getProject(intval($projectId));
       <h1><?= $project["title"] ?></h1>
       <p><?= $project["description"] ?></p>
     </article>
-    <h2>Leave a comment</h2>
-    <form action="/assets/php/contactUs.php" method="post">
+    <h2><?= $trad["form-item"]["h2"] ?></h2>
+    <form action="/assets/php/getComment.php" method="get">
       <div class="form-control">
-        <label for="surname"><?= $trad["contact-second-section"]["label1"] ?></label>
-        <input type="text" name="user-name" id="surname" placeholder="Prénom" />
+        <label for="surname"><?= $trad["form-item"]["labelPseudo"] ?></label>
+        <input type="text" name="user-name" id="surname" placeholder="Pseudo" />
         <i class="mdi mdi-check-circle-outline"></i>
         <i class="mdi mdi-alert-circle"></i>
         <small>Error message</small>
@@ -54,19 +62,20 @@ $project = $projects->getProject(intval($projectId));
 
 
       <div class="form-control">
-        <label for="user-msg"><?= $trad["contact-second-section"]["label4"] ?></label>
-        <textarea name="user-msg" id="user-msg" cols="30" rows="9" placeholder="Rédiger votre message"></textarea>
+        <label for="user-msg"><?= $trad["form-item"]["labelComment"] ?></label>
+        <textarea name="user-msg" id="user-msg" cols="30" rows="9" placeholder="Rédigez votre commentaire"></textarea>
         <small>Error message</small>
       </div>
-      <small>Your message has been successfully sent!</small>
-      <button><?= $trad["contact-second-section"]["submitBtn"] ?></button>
+      <small><?= $trad["form-item"]["SuccessNotification"] ?></small>
+      <button><?= $trad["form-item"]["submitBtn"] ?></button>
     </form>
   </main>
   <?php
   require_once './assets/templates/footer.php'
   ?>
 
-  <!-- <script src="./assets/js/contact.js"></script> -->
+  <script type="module" src="./assets/js/comment.js"></script>
+  <script src="./assets/js/shared-js.js?parent=projectItem.php"></script>
   <script src="./assets/js/main.js"></script>
 </body>
 

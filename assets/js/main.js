c1b68a40ae60c676575244f8cdf2bd8c47bfbd34
abdoul aziz diallo <abdoul-aziz.diallo@etu.unistra.fr>
2023-04-03 19:36:00 +0200

    Handle the burger menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,9 @@
 const hamburger = document.querySelector("header div");
 const navMenu = document.querySelector("header ul");
-const home = document.getElementById("home");
-const frontPage = document.querySelector("section:first-child");
+const bodyElt = document.querySelector("body");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
-  home.classList.toggle("change-top");
-  frontPage.classList.toggle("change-height");
+  bodyElt.classList.toggle("scrollOff");
 });

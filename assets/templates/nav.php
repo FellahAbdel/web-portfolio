bd625dddaf3a225ce1ffc387b7cf8638f16aaeaf
abdoul aziz diallo <abdoul-aziz.diallo@etu.unistra.fr>
2023-05-26 00:56:01 +0200

    Restore cv link and change css selector for the dropdown select

--- assets/templates/nav.php
+++ assets/templates/nav.php
@@ -5,7 +5,7 @@
           <li><a href="/admin.php"><?= $trad['nav']['admin'] ?><i class="mdi mdi-shield-crown"></i></a></li>
           <li><a href="/contact.php"><?= $trad['nav']['contact'] ?> <i class="mdi mdi-account-box"></i></a></li>
 
-          <li>
+          <li class="select">
             <select id="lang" name="lang">
               <option value="fr" <?php echo $fr_select ?>>Français</option>
               <option value="en" <?php echo $en_select ?>>English</option>

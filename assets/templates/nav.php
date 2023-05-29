e8fc5032629d91cc5e6d6b33ca5d7eb88c1a1d97
abdoul aziz diallo <abdoul-aziz.diallo@etu.unistra.fr>
2023-05-29 16:05:34 +0200

    Shinning nav bar

--- assets/templates/nav.php
+++ assets/templates/nav.php
@@ -2,7 +2,7 @@
         <ul>
           <li><a href="/index.php"><?= $trad['nav']['home'] ?> <i class="mdi mdi-home-circle"></i></a></li>
           <li><a href="/public/cvDiallo.pdf" target="_blank"><?= $trad['nav']['cv'] ?><i class="mdi mdi-file-account"></i></a></li>
-          <li><a href="/admin.php"><?= $trad['nav']['admin'] ?><i class="mdi mdi-shield-crown"></i></a></li>
+          <li><a href="/admin/"><?= $trad['nav']['admin'] ?><i class="mdi mdi-shield-crown"></i></a></li>
           <li><a href="/contact.php"><?= $trad['nav']['contact'] ?> <i class="mdi mdi-account-box"></i></a></li>
 
           <li class="select">

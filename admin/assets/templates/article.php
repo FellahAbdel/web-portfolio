735317eec13546574c646f30d49ea1d3dd46059a
abdoul aziz diallo <abdoul-aziz.diallo@etu.unistra.fr>
2023-05-29 23:12:20 +0200

    Minor change

--- admin/assets/templates/article.php
+++ admin/assets/templates/article.php
@@ -8,7 +8,7 @@
       </p>
     </div>
     <footer>
-      <a href="/edit.php?id=<?= $project["id"] ?>" class="btn btn-primary btn-lg"> <span class="mdi mdi-pencil"></span><?= $trad["adminActions"]["edit"] ?></a>
+      <a href="/update.php?id=<?= $project["id"] ?>" class="btn btn-primary btn-lg"> <span class="mdi mdi-pencil"></span><?= $trad["adminActions"]["edit"] ?></a>
       <a href="/delete.php?id=<?= $project["id"] ?>" class="btn btn-lg btn-danger"> <span class="mdi mdi-delete"></span> <?= $trad["adminActions"]["delete"] ?></a>
       <a href="/projectItem.php?id=<?= $project["id"] ?>" class="btn btn-dark btn-lg"> <span class="mdi mdi-arrow-right-thick"></span> <?= $trad["projectSection"]["exploreButton"] ?></a>
     </footer>

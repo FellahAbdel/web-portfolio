20dac8c33870f205122389085e1a17cea967a1f9
abdoul aziz diallo <abdoul-aziz.diallo@etu.unistra.fr>
2023-05-29 15:45:24 +0200

    Error are well handled for now

---
+++ admin/assets/js/formProject.js
@@ -0,0 +1,110 @@
+import {
+  setErrorFor,
+  setSuccessFor,
+  showSuccessfullMsg,
+  hideSuccesfullMsg,
+  removeSuccess,
+} from "../../../assets/js/shared-comment-contact.js";
+
+export function setErrorSpecif(input, message) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector("small");
+
+  // Add error message inside small
+  small.innerText = message;
+  console.log(small);
+  formControl.className = "file-upload-wrapper error";
+}
+
+export function setSuccessSpecif(input) {
+  const formControl = input.parentElement;
+  formControl.className = "file-upload-wrapper success";
+}
+
+const formElt = document.querySelector("form");
+const projectTitleElt = document.getElementById("project-title");
+const projectDescriptionElt = document.getElementById("description");
+const fileUploadWrapper = document.querySelector(".file-upload-wrapper");
+const projectImageElt = document.querySelector(".file-upload-field");
+
+let projectTitleValue;
+let projectDescriptionValue;
+let projectImageValue;
+
+formElt.addEventListener("change", function (event) {
+  // Update the field with the uploaded image name.
+  fileUploadWrapper.setAttribute("data-text", fileName);
+  var fileName = projectImageElt.value.replace(/.*(\/|\\)/, "");
+});
+
+formElt.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  // Vérification des données.
+  if (checkInputs()) {
+    showSuccessfullMsg();
+    const formData = new FormData(form);
+
+    fetch(
+      `${form.action}?id=${projectId}&pseudo-name=${encodeURIComponent(
+        projectTitleValue
+      )}&description=${encodeURIComponent(projectDescriptionValue)}`,
+      {
+        // include project ID in URL
+        method: "GET",
+      }
+    )
+      .then((response) => {
+        // Handle the response from the server
+        // ...
+        // Clear the form inputs
+        [projectTitleElt, projectDescriptionElt, projectImageElt].forEach((e) =>
+          removeSuccess(e)
+        );
+        form.reset();
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  } else {
+    hideSuccesfullMsg();
+  }
+});
+
+function checkInputs() {
+  // get the values from the inputs
+  let allInputsValid = true;
+  projectTitleValue = projectTitleElt.value.trim();
+  projectDescriptionValue = projectDescriptionElt.value.trim();
+
+  projectImageValue = projectImageElt.value.trim();
+  if (projectTitleValue === "") {
+    // show error
+    // add error class
+    allInputsValid = false;
+    setErrorFor(projectTitleElt, "Project title cannot be blank");
+    console.log(projectTitleElt);
+  } else {
+    // add sucess class
+    setSuccessFor(projectTitleElt);
+  }
+
+  if (projectDescriptionValue === "") {
+    allInputsValid = false;
+    setErrorFor(projectDescriptionElt, "Project description cannot be blank.");
+  } else if (projectDescriptionValue.length < 100) {
+    allInputsValid = false;
+    setErrorFor(projectDescriptionElt, "At least 100 characters");
+  } else {
+    setSuccessFor(projectDescriptionElt);
+  }
+
+  if (projectImageValue === "") {
+    allInputsValid = false;
+    setErrorSpecif(projectImageElt, "Project image is required.");
+  } else {
+    setSuccessSpecif(projectImageElt);
+  }
+
+  return allInputsValid;
+}

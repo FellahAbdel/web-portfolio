fc09607fc3b905c20d142c658cae2f12b84dd396
abdoul aziz diallo <abdoul-aziz.diallo@etu.unistra.fr>
2023-05-31 01:15:50 +0200

    Updating is working but not the redirection

---
+++ admin/assets/actions/update.php
@@ -0,0 +1,28 @@
+<?php
+if (!empty($_POST)) {
+  require_once __DIR__ . "/../../../assets/models/Projects.php";
+
+  $project = new Projects();
+  $projectName = $_POST["project-title"];
+  $projectDescription = $_POST["description"];
+  $textAltImg = $_POST["text-alt"];
+  $imageFile = $_FILES["file-upload-field"];
+
+  if (isset($_GET["id"])) {
+    $id = checkInput($_GET["id"]);
+    $success = $project->updateProject($id, $projectName, $projectDescription, $textAltImg, $imageFile);
+    if ($success) {
+      // After completing the project update, redirect to the index page
+      header("Location: insert.php"); // Replace "/index.php" with the actual URL of your index page
+      exit; // Make sure to exit after the redirect
+    }
+  }
+}
+
+function checkInput($data)
+{
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}

2599c992e61c2630f9a1df456fad0b8756b82546
abdoul aziz diallo <abdoul-aziz.diallo@etu.unistra.fr>
2023-06-01 00:27:42 +0200

    Minor changes on the form

--- admin/assets/templates/projectForm.php
+++ admin/assets/templates/projectForm.php
@@ -1,7 +1,7 @@
-<form role="form" action="/admin/assets/actions/insert.php" method="post" enctype="multipart/form-data">
+<form role="form" action="/admin/assets/actions/<?php echo $fileHandler; ?>" method="post" enctype="multipart/form-data">
   <div class="form-control">
     <label for="project-title"><?= $trad["AdminProjectForm"]["project title"] ?></label>
-    <input type="text" name="project-title" id="project-title" placeholder="<?= $trad["AdminProjectForm"]["project title"] ?>" />
+    <input type="text" name="project-title" id="project-title" placeholder="<?= $trad["AdminProjectForm"]["project title"] ?>" value="<?php echo $title ?>" />
     <i class="mdi mdi-check-circle-outline"></i>
     <i class="mdi mdi-alert-circle"></i>
     <small>Error message</small>
@@ -9,22 +9,20 @@
 
   <div class="form-control">
     <label for="description"><?= $trad["AdminProjectForm"]["description"] ?></label>
-    <textarea name="description" id="description" cols="30" rows="9" placeholder="<?= $trad["AdminProjectForm"]["description"] ?>"></textarea>
+    <textarea name="description" id="description" cols="30" rows="9" placeholder="<?= $trad["AdminProjectForm"]["description"] ?>"> <?php echo $description ?> </textarea>
     <small>Error message</small>
   </div>
   <div class="form-control">
     <label for="text-alt"><?= $trad["AdminProjectForm"]["text alt"] ?></label>
-    <input type="text" name="text-alt" id="text-alt" placeholder="<?= $trad["AdminProjectForm"]["text alt"] ?>" />
+    <input type="text" name="text-alt" id="text-alt" placeholder="<?= $trad["AdminProjectForm"]["text alt"] ?>" value="<?php echo $textAlt ?>" />
     <i class="mdi mdi-check-circle-outline"></i>
     <i class="mdi mdi-alert-circle"></i>
     <small>Error message</small>
   </div>
+  <img width="100px" src="/public/uploads/<?php echo $imageName ?>" alt="">
   <div class="file-upload-wrapper" data-text="<?= $trad["AdminProjectForm"]["project image"] ?>">
     <input name="file-upload-field" type="file" class="file-upload-field" value="">
     <small>Error message</small>
   </div>
-
-
-  <small>Project has been successfully added!</small>
-  <button><?= $trad["AdminProjectForm"]["upload button"] ?></button>
+  <button type="submit"><?php echo $trad["AdminProjectForm"][$button]; ?></button>
 </form>

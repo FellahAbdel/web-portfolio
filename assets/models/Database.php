7e1c2164ba31bf89bbef2eda9bffde6e583231d0
abdoul aziz diallo <abdoul-aziz.diallo@etu.unistra.fr>
2023-06-02 00:49:01 +0200

    Change way to connect to the db

--- assets/models/Database.php
+++ assets/models/Database.php
@@ -3,10 +3,14 @@
 abstract class Database
 {
   protected PDO $pdo;
+  private const HOST = 'mysql-fellah.alwaysdata.net';
+  private const DBNAME = 'fellah_database';
+  private const USER = 'fellah_phpmyadmi';
+  private const PASS = 'famillediallo';
 
   public function __construct()
   {
-    $this->pdo = new PDO('sqlite:' . dirname(__FILE__) . '/database.db');
+    $this->pdo = new PDO('mysql:host=' . Database::HOST . ';dbname=' . Database::DBNAME, Database::USER, Database::PASS);
     $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   }

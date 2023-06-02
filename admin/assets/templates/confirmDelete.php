2b0ccf739a492e6bc8655b6f323b868136028790
abdoul aziz diallo <abdoul-aziz.diallo@etu.unistra.fr>
2023-06-02 23:39:52 +0200

    delete  var no need

--- admin/assets/templates/confirmDelete.php
+++ admin/assets/templates/confirmDelete.php
@@ -2,7 +2,7 @@
   <input type="hidden" name="project-id" value="<?php echo $projectId ?>">
   <p class="alert alert-warning"><?= $trad["AdminProjectForm"]["question"] ?></p>
   <div class="form-group">
-    <button type="submit" class="btn btn-warning"><?php echo $trad["AdminProjectForm"][$button]; ?><?= $trad["AdminProjectForm"]["y-response"] ?></button>
+    <button type="submit" class="btn btn-warning"><?= $trad["AdminProjectForm"]["y-response"] ?></button>
     <a href="/admin/index.php" class="btn btn-dark btn-lg"><?= $trad["AdminProjectForm"]["n-response"] ?></a>
   </div>
 </form>

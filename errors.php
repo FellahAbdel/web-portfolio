2361265be16792a2ec615ab5a56e106bb9aeb5fb
abdoul aziz diallo <abdoul-aziz.diallo@etu.unistra.fr>
2023-06-22 23:42:32 +0200

    Handle errors

--- errors.php
+++ errors.php
@@ -5,7 +5,11 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Error</title>
-  <link rel="stylesheet" href="assets/css/errors.css" />
+  <style>
+    body {
+      background-color: black;
+    }
+  </style>
 </head>
 
 <body>

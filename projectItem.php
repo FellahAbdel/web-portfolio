9913d75e43add10773e79e7e31db453161540cfe
fellah <abdoulazizdiallofouta@gmail.com>
2024-01-23 14:08:17 +0100

    No need

--- projectItem.php
+++ projectItem.php
@@ -43,31 +43,7 @@ function checkInput($data)
   <link rel="stylesheet" href="assets/css/project-item.css" />
   <link rel="stylesheet" href="assets/css/form-item.css" />
   <!-- <link rel="stylesheet" href="/admin/assets/css/bootstrapp.css" /> -->
-  <script src="https://cdn.tiny.cloud/1/9vn6cnyc2nhzsn1onc2crevikcwxivlfrir4y43qht5odb8x/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
 
-  <!-- Place the following <script> and <textarea> tags your HTML's <body> -->
-  <script>
-    tinymce.init({
-      selector: "textarea",
-      plugins: "ai tinycomments mentions anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed permanentpen footnotes advtemplate advtable advcode editimage tableofcontents mergetags powerpaste tinymcespellchecker autocorrect a11ychecker typography inlinecss",
-      toolbar: "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | align lineheight | tinycomments | checklist numlist bullist indent outdent | emoticons charmap | removeformat",
-      tinycomments_mode: "embedded",
-      tinycomments_author: "Author name",
-      mergetags_list: [{
-          value: "First.Name",
-          title: "First Name"
-        },
-        {
-          value: "Email",
-          title: "Email"
-        },
-      ],
-      ai_request: (request, respondWith) =>
-        respondWith.string(() =>
-          Promise.reject("See docs to implement AI Assistant")
-        ),
-    });
-  </script>
   <title>Un projet</title>
 </head>
 

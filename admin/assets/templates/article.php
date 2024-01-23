a1e6a886d7c44d4a7f9914c697659be20fa25465
fellah <abdoulazizdiallofouta@gmail.com>
2024-01-23 14:57:25 +0100

    Minor change

--- admin/assets/templates/article.php
+++ admin/assets/templates/article.php
@@ -4,7 +4,7 @@
     <div>
       <h2><?= $project["title"] ?> </h2>
       <p>
-        <?= htmlspecialchars_decode(substr($project["description"], 0, 300)) ?>
+        <?= htmlspecialchars_decode(substr($project["description"], 0, 400)) ?>...
       </p>
     </div>
     <footer>

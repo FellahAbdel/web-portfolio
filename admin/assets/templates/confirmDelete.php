dd3e089c0d1594c4e868cfc8b6676d03ee0a6ab1
fellah <abdoulazizdiallofouta@gmail.com>
2024-02-01 14:58:55 +0100

    delete admin from remote

--- admin/assets/templates/confirmDelete.php
+++
@@ -1,8 +0,0 @@
-<form role="form" action="/admin/assets/actions/delete.php" method="post">
-  <input type="hidden" name="project-id" value="<?php echo $projectId ?>">
-  <p class="alert alert-warning"><?= $trad["AdminProjectForm"]["question"] ?></p>
-  <div>
-    <button type="submit" class="btn btn-warning"><?= $trad["AdminProjectForm"]["y-response"] ?></button>
-    <a href="/admin/index.php" class="btn btn-dark btn-lg"><?= $trad["AdminProjectForm"]["n-response"] ?></a>
-  </div>
-</form>

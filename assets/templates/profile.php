7ac087880beb9b813f8cfcc4bd6873f6eca912a5
FellahAbdel <abdoulazizdiallofouta@gmail.com>
2026-06-03 16:13:59 +0200

    new profil photo

--- assets/templates/profile.php
+++ assets/templates/profile.php
@@ -1,4 +1,4 @@
         <div>
           <!-- Image Avatar Premium -->
-          <img src="./assets/images/DSC_0590.png" alt="Profile Picture" />
+          <img src="./assets/images/new-profile-photo.png" alt="Profile Picture" />
         </div>

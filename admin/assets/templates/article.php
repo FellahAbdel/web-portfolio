3278d99d2a4e4327de5fba74d8276a517c30039f
FellahAbdel <abdoulazizdiallofouta@gmail.com>
2026-08-19 22:30:56 +0200

    feat(admin): redesign projects page cards with glassmorphism design system

--- admin/assets/templates/article.php
+++ admin/assets/templates/article.php
@@ -1,16 +1,32 @@
-<li>
-  <img src="/public/uploads/<?= $project["imageName"] ?>" alt="<?= $project["imageAlt"] ?>" />
-  <article>
-    <div>
-      <h2><?= $project["title"] ?> </h2>
-      <p>
-        <?= htmlspecialchars_decode(substr($project["description"], 0, 400)) ?>...
-      </p>
+<li class="project-card admin-project-card">
+  <div class="project-image-wrapper">
+    <img src="/public/uploads/<?= $project["imageName"] ?>" alt="<?= $project["imageAlt"] ?>" loading="lazy" />
+    <div class="project-overlay">
+      <a href="/projectItem.php?id=<?= $project["id"] ?>" class="btn-view-project" target="_blank">
+        <?= $trad["projectSection"]["viewButton"] ?> <i class="mdi mdi-arrow-right"></i>
+      </a>
     </div>
-    <footer>
-      <a href="/admin/update.php?id=<?= $project["id"] ?>" class="btn btn-primary btn-lg"> <span class="mdi mdi-pencil"></span><?= $trad["adminActions"]["edit"] ?></a>
-      <a href="/admin/delete.php?id=<?= $project["id"] ?>" class="btn btn-lg btn-danger"> <span class="mdi mdi-delete"></span> <?= $trad["adminActions"]["delete"] ?></a>
-      <a href="/projectItem.php?id=<?= $project["id"] ?>" class="btn btn-dark btn-lg"> <span class="mdi mdi-arrow-right-thick"></span> <?= $trad["projectSection"]["viewButton"] ?></a>
-    </footer>
-  </article>
+  </div>
+  <div class="project-content">
+    <div class="project-header">
+      <h3><?= $project["title"] ?></h3>
+      <span class="admin-project-id">#<?= $project["id"] ?></span>
+    </div>
+    <p>
+      <?= htmlspecialchars_decode(substr($project["description"], 0, 120)) ?>...
+    </p>
+    <div class="admin-project-actions">
+      <a href="/admin/update.php?id=<?= $project["id"] ?>" class="admin-action-btn admin-action-edit">
+        <i class="mdi mdi-pencil"></i>
+        <span><?= $trad["adminActions"]["edit"] ?></span>
+      </a>
+      <a href="/projectItem.php?id=<?= $project["id"] ?>" class="admin-action-btn admin-action-view" target="_blank">
+        <i class="mdi mdi-arrow-top-right"></i>
+        <span><?= $trad["projectSection"]["viewButton"] ?></span>
+      </a>
+      <a href="/admin/delete.php?id=<?= $project["id"] ?>" class="admin-action-btn admin-action-delete">
+        <i class="mdi mdi-delete"></i>
+      </a>
+    </div>
+  </div>
 </li>
